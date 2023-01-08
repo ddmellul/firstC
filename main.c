e02a2bccef982a11b91ed87da06e06a1537836e7
ddmellul <40177000+ddmellul@users.noreply.github.com>
2023-01-08 10:11:20 +0200

    Create main.c
    this first file will have future changes. adding functions and others. all the purpose is to write simple C code but use GITHUB to manage ( upgrade , debug and commit ) this file with some mates

---
+++ main.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+DATE    :08.01.2023
+ID      :DDM
+DESC    :first program to be located in Github
+
+REVISION    DATE        by      Description
+     000    08.01.23    DM      one printf instruction
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello GIT");
+
+    return 0;
+}
