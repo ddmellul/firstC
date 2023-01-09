18520f0aad10e695fb3619fdd29b037906965b7b
ddmellul <40177000+ddmellul@users.noreply.github.com>
2023-01-09 08:32:35 +0200

    Update main.c
    001       09.01.23     DM     PrintGreeting() function added by david@sapir

--- main.c
+++ main.c
@@ -3,14 +3,21 @@ DATE    :08.01.2023
 ID      :DDM
 DESC    :first program to be located in Github
 
-REVISION    DATE        by      Description
-     000    08.01.23    DM      one printf instruction
+   REVISION      DATE       by          Description
+     000       08.01.23     DM     one printf instruction
+     001       09.01.23     DM     PrintGreeting() function added
 *******************************************************************************/
 #include <stdio.h>
+
+void PrintGreeting();
 
 int main()
 {
      printf("Hello GIT");
-      
+     PrintGreeting(); 
     return 0;
+}
+
+void PrintGreeting(){
+     printf("\nHello GIT\n");
 }
